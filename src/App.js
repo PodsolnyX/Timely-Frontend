bb5fb881c79ea8d7b557128ac047ca59d0f2424e
Yuraev Aleksandr <podsolnyx03@mail.ru>
2023-02-15 15:30:41 +0700

    create basic react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className={"App"}>
+          <div className={"brand"}>
+              <h1 >Timely</h1>
+              <p>[WORK IN PROGRESS]</p>
+          </div>
+      </div>
+  );
+}
+
+export default App;
