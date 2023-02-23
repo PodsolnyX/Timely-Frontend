af5222e293e41a179e7e56b2b3849459e41342a2
DragonGames85 <dragongames85@mail.ru>
2023-02-23 18:20:43 +0700

    add search entities and methods in zustand store

--- src/shared/useZustandStore.jsx
+++ src/shared/useZustandStore.jsx
@@ -1,54 +1,108 @@
 import { create } from "zustand";
+import axios from "axios";
+
+const searchApi = "http://timely.markridge.space/api/search/";
 
 const initialState = {
   isAuth: false,
   isLoading: false,
   error: "",
-  user: {
-    name: "",
-    role: [],
-    group: 0,
+  profile: {
+    fullName: "",
+    userName: "",
     email: "",
+    group: {
+      id: "",
+      name: "",
+    },
+    teacher: {
+      id: "",
+      name: "",
+    },
+    roles: [],
   },
-  
 
-
+  shedule: {},
+  teachers: [],
+  groups: [],
+  classrooms: [],
+  timeIntervals: [],
+  lessonNames: [],
+  lessonTags: [],
 };
 
 export const useZustandStore = create(set => ({
   ...initialState,
-  login: (email, password) => {
-    set((user.isLoading = true));
-    // axios.get("https://www.database")..........
-    // if (response.ok) {error = "", user = data.user, set(isAuth=true)}
-    // else {error = data.error}
-    set((user.isLoading = false));
-    location.reload();
+  getGroups: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}groups`);
+      const groups = response.data; 
+      set({ groups: groups, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, groups: [] });
+    } finally {
+      set({ isLoading: false });
+    }
   },
-  register: (name, role, group, email) => {
-    set((user.isLoading = true));
-    // axios.post("https://www.database")..........
-    // if (response.ok) {error = "", user = data.user, set(isAuth=true)}
-    // else {error = data.error}
-    // localStorage.setItem("auth", true)
-    // localStorage.setItem("user", data.user)
-    set((user.isLoading = false));
-    location.reload();
+  getTeachers: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}teachers`);
+      const teachers = response.data; 
+      set({ teachers: teachers, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, teachers: [] }); // Update the state with the error message and clear the groups and isAuth
+    } finally {
+      set({ isLoading: false });
+    }
   },
-  logout: () => {
-    // localStorage.delete("auth")
-    // localStorage.delete("user")
-    location.reload();
+  getClassrooms: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}classrooms`);
+      const classrooms = response.data; 
+      set({ classrooms: classrooms, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, classrooms: [] }); // Update the state with the error message and clear the groups and isAuth
+    } finally {
+      set({ isLoading: false });
+    }
   },
-
-// пока не трогать
-//   confirmMail: () => {},
-//   changeProfile: () => {},
-  
-// // админ
-//   addPair: () => {},
-//   deletePair: () => {},
-//   changePair: () => {},
-//   adminChangeEmail: () => {},
-//   adminCreateTeacher
+  getTimeIntervals: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}timeIntervals`);
+      const timeIntervals = response.data; 
+      set({ timeIntervals: timeIntervals, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, timeIntervals: [] }); // Update the state with the error message and clear the groups and isAuth
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  getLessonNames: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}lessonNames`);
+      const lessonNames = response.data; 
+      set({ lessonNames: lessonNames, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, lessonNames: [] }); // Update the state with the error message and clear the groups and isAuth
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  getLessonTags: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await axios.get(`${searchApi}lessonTags`);
+      const lessonTags = response.data; 
+      set({ lessonTags: lessonTags, error: "" }); 
+    } catch (error) {
+      set({ error: error.message, lessonTags: [] }); // Update the state with the error message and clear the groups and isAuth
+    } finally {
+      set({ isLoading: false });
+    }
+  },
 }));
