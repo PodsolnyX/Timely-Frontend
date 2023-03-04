1bf588562e3ea9a4a56976633031ea66b660b704
Gritzaev Nikita <gritzaevnikita@gmail.com>
2023-03-04 14:47:44 +0700

    add lesson error

--- src/shared/useZustandStore.js
+++ src/shared/useZustandStore.js
@@ -1,31 +1,31 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import axios from "axios";
-import {sheduleMatrix} from "../helpers/sheduleMatrix";
-import {useZustandFormStore} from './useZustandFormStore';
+import { sheduleMatrix } from "../helpers/sheduleMatrix";
+import { useZustandFormStore } from "./useZustandFormStore";
 
 const initialState = {
-    isAuth: !!localStorage.getItem("jwt"),
-    isLoading: false,
-    isLoadingSchedule: false,
-    error: "",
-    profile: JSON.parse(localStorage.getItem("profile")) || {},
+  isAuth: !!localStorage.getItem("jwt"),
+  isLoading: false,
+  isLoadingSchedule: false,
+  error: "",
+  lessonError: "",
+  profile: JSON.parse(localStorage.getItem("profile")) || {},
 
-    groupSchedule: null,
-    classroomSchedule: {},
-    teacherSchedule: {},
+  groupSchedule: null,
+  classroomSchedule: {},
+  teacherSchedule: {},
 
-    teachers: [],
-    groups: [],
-    classrooms: [],
+  teachers: [],
+  groups: [],
+  classrooms: [],
 
-    timeIntervals: [],
-    lessonNames: [],
-    lessonTags: [],
-
+  timeIntervals: [],
+  lessonNames: [],
+  lessonTags: [],
 };
 
-export const useZustandStore = create(set => ({
-    ...initialState,
+export const useZustandStore = create((set) => ({
+  ...initialState,
 
   getTeacherSchedule: async (date, teacherID) => {
     set({ isLoading: true });
@@ -111,7 +111,7 @@ export const useZustandStore = create(set => ({
     try {
       const response = await axios.get(`search/groups`);
       const groups = response.data;
-      groups.forEach(el => {
+      groups.forEach((el) => {
         delete Object.assign(el, {
           ["value"]: el["id"],
           ["label"]: el["name"],
@@ -130,7 +130,7 @@ export const useZustandStore = create(set => ({
     try {
       const response = await axios.get(`search/teachers`);
       const teachers = response.data;
-      teachers.forEach(el => {
+      teachers.forEach((el) => {
         delete Object.assign(el, {
           ["value"]: el["id"],
           ["label"]: el["name"],
@@ -149,7 +149,7 @@ export const useZustandStore = create(set => ({
     try {
       const response = await axios.get(`search/classrooms`);
       const classrooms = response.data;
-      classrooms.forEach(el => {
+      classrooms.forEach((el) => {
         delete Object.assign(el, {
           ["value"]: el["id"],
           ["label"]: el["name"],
@@ -180,7 +180,7 @@ export const useZustandStore = create(set => ({
     try {
       const response = await axios.get(`search/lessonNames`);
       const lessonNames = response.data;
-      lessonNames.forEach(el => {
+      lessonNames.forEach((el) => {
         delete Object.assign(el, {
           ["value"]: el["id"],
           ["label"]: el["name"],
@@ -199,7 +199,7 @@ export const useZustandStore = create(set => ({
     try {
       const response = await axios.get(`search/lessonTags`);
       const lessonTags = response.data;
-      lessonTags.forEach(el => {
+      lessonTags.forEach((el) => {
         delete Object.assign(el, {
           ["value"]: el["id"],
           ["label"]: el["name"],
@@ -214,155 +214,156 @@ export const useZustandStore = create(set => ({
     }
   },
 
-    register: async (email, password, fullName) => {
-        set({isLoading: true});
-        try {
-            const request = await axios.post("account/register", {
-                fullName,
-                email,
-                password,
-            });
-            localStorage.setItem("jwt", request.data.token);
-            set({isAuth: true});
-        } finally {
-            set({isLoading: false});
-        }
-    },
-    sendEmail: async () => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.post("account/send-email-confirmation", null, {
-            headers: {
-                Authorization: `Bearer ${jwt}`,
-            },
+  register: async (email, password, fullName) => {
+    set({ isLoading: true });
+    try {
+      const request = await axios.post("account/register", {
+        fullName,
+        email,
+        password,
+      });
+      localStorage.setItem("jwt", request.data.token);
+      set({ isAuth: true });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  sendEmail: async () => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.post("account/send-email-confirmation", null, {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+  },
+  confirmEmail: async (token) => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.post(
+      "account/confirm-email",
+      {
+        token,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+  },
+  login: async (email, password) => {
+    set({ isLoading: true });
+    try {
+      const request = await axios.post("account/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("jwt", request.data.token);
+      set({ isAuth: true });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  logout: async (force = false) => {
+    const jwt = localStorage.getItem("jwt");
+    localStorage.removeItem("jwt");
+    try {
+      if (!force) {
+        await axios.post("account/logout", null, {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
         });
-    },
-    confirmEmail: async token => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.post(
-            "account/confirm-email",
-            {
-                token,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }
-        );
-    },
-    login: async (email, password) => {
-        set({isLoading: true});
-        try {
-            const request = await axios.post("account/login", {
-                email,
-                password,
-            });
-            localStorage.setItem("jwt", request.data.token);
-            set({isAuth: true});
-        } finally {
-            set({isLoading: false});
-        }
-    },
-    logout: async (force = false) => {
-        const jwt = localStorage.getItem("jwt");
-        localStorage.removeItem("jwt");
-        try {
-            if (!force) {
-                await axios.post("account/logout", null, {
-                    headers: {
-                        "Authorization": `Bearer ${jwt}`
-                    }
-                })
-            }
-        } finally {
-            useZustandFormStore.getState().reset();
-            set({isAuth: false});
-        }
-    },
-    editProfile: async fullName => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.put(
-            "account/profile",
-            {
-                fullName,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }
-        );
-    },
-    changePassword: async (currentPassword, newPassword) => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.put(
-            "account/password-change",
-            {
-                currentPassword,
-                newPassword,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }
-        );
-    },
-    setGroup: async groupId => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.put(
-            "account/group/set",
-            {
-                groupId,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }
-        );
-    },
-    removeGroup: async () => {
-        const jwt = localStorage.getItem("jwt");
-        await axios.delete("account/group/remove", {
-            headers: {
-                Authorization: `Bearer ${jwt}`,
-            },
-        });
-    },
+      }
+    } finally {
+      useZustandFormStore.getState().reset();
+      set({ isAuth: false });
+    }
+  },
+  editProfile: async (fullName) => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.put(
+      "account/profile",
+      {
+        fullName,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+  },
+  changePassword: async (currentPassword, newPassword) => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.put(
+      "account/password-change",
+      {
+        currentPassword,
+        newPassword,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+  },
+  setGroup: async (groupId) => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.put(
+      "account/group/set",
+      {
+        groupId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+  },
+  removeGroup: async () => {
+    const jwt = localStorage.getItem("jwt");
+    await axios.delete("account/group/remove", {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+  },
 
-    createTeacher: async name => await admin("create", "teacher", {name}),
-    editTeacher: async (id, name) => await admin("edit", "teacher", id, {name}),
-    deleteTeacher: async id => await admin("delete", "teacher", id),
+  createTeacher: async (name) => await admin("create", "teacher", { name }),
+  editTeacher: async (id, name) => await admin("edit", "teacher", id, { name }),
+  deleteTeacher: async (id) => await admin("delete", "teacher", id),
 
-    createDomain: async url => await admin("create", "domain", {url}),
-    editDomain: async (id, url) => await admin("edit", "domain", id, {url}),
-    deleteDomain: async id => await admin("delete", "domain", id),
+  createDomain: async (url) => await admin("create", "domain", { url }),
+  editDomain: async (id, url) => await admin("edit", "domain", id, { url }),
+  deleteDomain: async (id) => await admin("delete", "domain", id),
 
-    createClassroom: async name => await admin("create", "classroom", {name}),
-    editClassroom: async (id, name) =>
-        await admin("edit", "classroom", id, {name}),
-    deleteClassroom: async id => await admin("delete", "classroom", id),
+  createClassroom: async (name) => await admin("create", "classroom", { name }),
+  editClassroom: async (id, name) =>
+    await admin("edit", "classroom", id, { name }),
+  deleteClassroom: async (id) => await admin("delete", "classroom", id),
 
-    createGroup: async name => await admin("create", "group", {name}),
-    editGroup: async (id, name) => await admin("edit", "group", id, {name}),
-    deleteGroup: async id => await admin("delete", "group", id),
+  createGroup: async (name) => await admin("create", "group", { name }),
+  editGroup: async (id, name) => await admin("edit", "group", id, { name }),
+  deleteGroup: async (id) => await admin("delete", "group", id),
 
-    createLessonName: async name => await admin("create", "lessonName", {name}),
-    editLessonName: async (id, name) =>
-        await admin("edit", "lessonName", id, {name}),
-    deleteLessonName: async id => await admin("delete", "lessonName", id),
+  createLessonName: async (name) =>
+    await admin("create", "lessonName", { name }),
+  editLessonName: async (id, name) =>
+    await admin("edit", "lessonName", id, { name }),
+  deleteLessonName: async (id) => await admin("delete", "lessonName", id),
 
-    createLessonTag: async name => await admin("create", "lessonTag", {name}),
-    editLessonTag: async (id, name) =>
-        await admin("edit", "lessonTag", id, {name}),
-    deleteLessonTag: async id => await admin("delete", "lessonTag", id),
+  createLessonTag: async (name) => await admin("create", "lessonTag", { name }),
+  editLessonTag: async (id, name) =>
+    await admin("edit", "lessonTag", id, { name }),
+  deleteLessonTag: async (id) => await admin("delete", "lessonTag", id),
 
-    createTimeInterval: async (startTime, endTime) =>
-        await admin("create", "timeInterval", {startTime, endTime}),
-    editTimeInterval: async (id, startTime, endTime) =>
-        await admin("edit", "timeInterval", id, {startTime, endTime}),
-    deleteTimeInterval: async id => await admin("delete", "timeInterval", id),
+  createTimeInterval: async (startTime, endTime) =>
+    await admin("create", "timeInterval", { startTime, endTime }),
+  editTimeInterval: async (id, startTime, endTime) =>
+    await admin("edit", "timeInterval", id, { startTime, endTime }),
+  deleteTimeInterval: async (id) => await admin("delete", "timeInterval", id),
 
   createLesson: async (
     nameId,
@@ -372,16 +373,22 @@ export const useZustandStore = create(set => ({
     timeIntervalId,
     date,
     classroomId
-  ) =>
-    await admin("create", "lesson", {
-      nameId,
-      tagId,
-      groupId,
-      teacherId,
-      timeIntervalId,
-      date,
-      classroomId,
-    }),
+  ) => {
+    try {
+      set({ lessonError: "" });
+      await admin("create", "lesson", {
+        nameId,
+        tagId,
+        groupId,
+        teacherId,
+        timeIntervalId,
+        date,
+        classroomId,
+      });
+    } catch (error) {
+      set({ lessonError: error.response.data.title });
+    }
+  },
   editLesson: async (
     id,
     nameId,
@@ -391,39 +398,45 @@ export const useZustandStore = create(set => ({
     timeIntervalId,
     date,
     classroomId
-  ) =>
-    await admin("edit", "lesson", id, {
-      nameId,
-      tagId,
-      groupId,
-      teacherId,
-      timeIntervalId,
-      date,
-      classroomId,
-    }),
-  deleteLesson: async id => await admin("delete", "lesson", id),
+  ) => {
+    try {
+      set({ lessonError: "" });
+      await admin("edit", "lesson", id, {
+        nameId,
+        tagId,
+        groupId,
+        teacherId,
+        timeIntervalId,
+        date,
+        classroomId,
+      });
+    } catch (error) {
+      set({ lessonError: error.response.data.title });
+    }
+  },
+  deleteLesson: async (id) => await admin("delete", "lesson", id),
 }));
 
 async function admin(action, ...params) {
-    const jwt = localStorage.getItem("jwt");
-    const headers = {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-    };
-    switch (action) {
-        case "create":
-            await axios.post(`admin/${params[0]}/create`, params[1], headers);
-            break;
-        case "edit":
-            await axios.put(
-                `admin/${params[0]}/edit/${params[1]}`,
-                params[2],
-                headers
-            );
-            break;
-        case "delete":
-            await axios.delete(`admin/${params[0]}/delete/${params[1]}`, headers);
-            break;
-    }
+  const jwt = localStorage.getItem("jwt");
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  };
+  switch (action) {
+    case "create":
+      await axios.post(`admin/${params[0]}/create`, params[1], headers);
+      break;
+    case "edit":
+      await axios.put(
+        `admin/${params[0]}/edit/${params[1]}`,
+        params[2],
+        headers
+      );
+      break;
+    case "delete":
+      await axios.delete(`admin/${params[0]}/delete/${params[1]}`, headers);
+      break;
+  }
 }
