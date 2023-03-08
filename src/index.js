16d8e88f4475d2f8ce08550685e3a5d8372e8a01
Yuraev Aleksandr <podsolnyx03@mail.ru>
2023-03-08 13:19:05 +0700

    fix router ver.3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={"/Timely-Frontend"}>
           <App />
       </BrowserRouter>
   </React.StrictMode>
