64bb8dde086c5a4ef489df7ea81f77718eb48685
Yuraev Aleksandr <podsolnyx03@mail.ru>
2023-03-08 14:40:57 +0700

    fix routing for git-hub pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={"/Timely-Frontend"}>
           <App />
       </BrowserRouter>
   </React.StrictMode>
