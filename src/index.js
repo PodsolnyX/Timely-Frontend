5574e30557614e79b023b149b667562a1f9f3fb4
Yuraev Aleksandr <podsolnyx03@mail.ru>
2023-03-08 12:49:44 +0700

    fix router ver.2

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
